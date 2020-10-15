dbbfc1be3d514b99a360ec8526d2d604dd9b160a
Javier Moreno <mq2880@gmail.com>
2020-10-15 12:21:47 +0200

    Commit

---
+++ EventsAndXAML/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+struct Coffee
+{
+    public string Name { get; set; }
+    public string Bean { get; set; }
+    public string CountryOfOrigin { get; set; }
+    public int Strength { get; set; }
+}
+namespace EventsAndXAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Coffee coffee1 = new Coffee();
+            coffee1.Name = "Fourth Coffee Quencher";
+            coffee1.CountryOfOrigin = "Indonesia";
+            coffee1.Strength = 3;
+
+            Console.WriteLine("Name: {0}", coffee1.Name);
+            Console.WriteLine("Country of Origin: {0}", coffee1.CountryOfOrigin);
+            Console.WriteLine("Strength: {0}", coffee1.Strength);
+        }
+
+        private void btnGetTime_Click(object sender, RoutedEventArgs e)
+        {
+            lblShowTime.Content = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 20483C-Mod3-Demo-Lesson1
+# Module 3:  Developing the Code for a Graphical Application
+
+**Lesson 3:  Handling Events**
+
+**Demonstration: Working with Events in XAML**
+
+- Pasos
+
+  - En el evento click del boton añadir el codigo
+
+    ![](C:\Users\mq288\Downloads\Mod2-Demo-Lesson3-1.jpg)
+
+  - Ver que VS ha añadido un tag al elemento boton
+
+    ![](C:\Users\mq288\Downloads\Mod2-Demo-Lesson3-2.jpg)
+
+  - Compilar y ver que se ha añadido en el fichero MainWindows.g.cs el siguiente codigo:
+
+    ![](C:\Users\mq288\Downloads\Mod2-Demo-Lesson3-3.jpg)
